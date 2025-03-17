b59a2fd5778a578df07445dcb48dd9dd11a9f5c6
Mykola Kryvoruchko <krivoruchko.kolya@gmail.com>
2025-03-17 22:30:06 +0200

    init db

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
 import React from "react";
 
 const RootPage = () => {
-  return <div className="text-[var(--green-color)]">RootPage</div>;
+  return (
+    <div className="text-[var(--green-color)]">
+      Настя тут може бути твоя реклама
+      <Image src="/images/nastya.jpg" width={1000} height={1000} alt="image" />
+    </div>
+  );
 };
 
 export default RootPage;
